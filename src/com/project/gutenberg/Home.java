7337be7d28d8dacc2148e0f2a9721c41abf3e7b3
David Breneisen <dbreneisen@gmail.com>
2013-03-30 19:10:34 -0400

    Setting up utilities

--- src/com/project/gutenberg/Home.java
+++ src/com/project/gutenberg/Home.java
@@ -21,7 +21,7 @@ public class Home extends RootActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initialize_app();
-                 //    /
+                 //    //
 
     }
 

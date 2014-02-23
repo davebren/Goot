63623deeb90a66cf62a3e1067c3bec4e243d2f1b
Eskimo Apps <eskimoapps@gmail.com>
2014-02-23 18:16:37 -0500

    adds a method parseBook(ResponseCallback) to BookParser for parsing off the UI thread.

--- app/src/com/project/gutenberg/book/parsing/BookParser.java
+++ app/src/com/project/gutenberg/book/parsing/BookParser.java
@@ -1,11 +1,13 @@
 package com.project.gutenberg.book.parsing;
 
 import com.project.gutenberg.book.Book;
+import com.project.gutenberg.util.ResponseCallback;
 
 import java.util.LinkedList;
 
 
 public interface BookParser {
     Book parseBook();
+    void parseBook(ResponseCallback<Book> callback);
     LinkedList<String> parseChapter(int chapter_index);
 }
